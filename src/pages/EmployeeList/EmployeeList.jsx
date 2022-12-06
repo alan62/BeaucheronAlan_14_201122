74658709a685ba130e8b64e19d0a32fa9f84fb41
Alan62 <beaucheron.alan@free.fr>
2022-12-07 00:49:13 +0100

    création page employee

---
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+import DataTable from '../../components/DataTable/DataTable'
+
+/**
+ * EmployeeList page
+ * @returns {React.ReactElement} JSX.Element - the EmployeeList page
+ */
+
+function EmployeeList() {
+
+  return (
+    <main className="main__employees">
+      <div className="container mb-2 py-2 border rounded">
+      <DataTable />
+      </div>
+      <div className="text-center">
+        <Link className="" to="/">
+          <button type="button" className="btn btn-primary">
+            {' '}
+            Home
+          </button>
+        </Link>
+      </div>
+    </main>
+  )
+}
+
+export default EmployeeList
