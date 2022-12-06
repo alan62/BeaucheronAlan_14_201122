12ff293b971f36761aa482a34f32a2289a7d8114
Alan62 <beaucheron.alan@free.fr>
2022-12-07 00:52:16 +0100

    component datatable ( tableau generé avec euka-datatables ) + css

---
+++ src/components/DataTable/DataTable.jsx
@@ -0,0 +1,83 @@
+import EukaDataTable from 'euka-datatables'
+import { useGlobalState } from '../../state/State'
+import { useState } from 'react'
+import deleteUser from '../../utils/deleteUser'
+import "./dataTable.css"
+
+/**
+ * DataTable component
+ * @returns {React.ReactElement} JSX.Element - the list of employees
+ */
+
+export default function DataTable() {
+  const selected = document.querySelector('.selected-info')
+ 
+  const [employees, setEmployees] = useGlobalState('employee')
+  const [employeesListToDelete, setEmployeesListToDelete] = useState()
+
+  deleteUser(selected, employeesListToDelete, employees, setEmployees)
+
+  let columns = [
+    {
+      name: 'firstName',
+      label: 'First Name',
+    },
+    {
+      name: 'lastName',
+      label: 'Last Name',
+    },
+    {
+      name: 'startDate',
+      label: 'Start Date',
+    },
+    {
+      name: 'department',
+      label: 'Department',
+    },
+    {
+      name: 'dateOfBirth',
+      label: 'Date of Birth',
+    },
+    {
+      name: 'street',
+      label: 'Street',
+    },
+    {
+      name: 'city',
+      label: 'City',
+    },
+    {
+      name: 'state',
+      label: 'State',
+    },
+    {
+      name: 'zipCode',
+      label: 'Zip Code',
+    },
+  ]
+
+  let options = {
+    responsive: 'collapse',
+    recordsPerPageOptions: { 10: 10, 25: 25, 50: 50, 100: 100 },
+    selectRows: true,
+    onRowsSelect: (selectedDataIndices, selectedData, parentRecord) => {
+      'selectedData'
+
+      setEmployeesListToDelete(selectedData)
+    },
+  }
+
+  return (
+    <>
+      <section className="dataTable">
+        <h2 className="dataTable__employeeTitle">Current Employees</h2>
+        <EukaDataTable
+          key={'table-1'}
+          columns={columns}
+          data={employees}
+          options={options}
+        />
+      </section>
+    </>
+  )
+}
