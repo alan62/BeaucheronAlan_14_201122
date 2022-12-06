54b284628ba2b60e3845ea3b9c301a122ae6a559
Alan62 <beaucheron.alan@free.fr>
2022-12-07 00:48:05 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import EmployeeList from './pages/EmployeeList/EmployeeList'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/EmployeeList"
+            element={<EmployeeList />}
+          />
+        </Routes>
+      <Footer />
+    </Router>
   );
 }
 
