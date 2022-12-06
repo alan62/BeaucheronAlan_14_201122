96909b43995952e22e93872ebdc014b7fc64b1bf
Alan62 <beaucheron.alan@free.fr>
2022-12-07 00:55:20 +0100

    fonction handle change option

---
+++ src/utils/handleChangeOption.js
@@ -0,0 +1,25 @@
+/**
+ * Function to handle change option
+ * @param {*} e - the event
+ * @param {*} setEmployeeFromData  - function to set the state of the input fields
+ * @param {object} employeeFromData - object with the input fields values
+ * @param {String} employeeFromData.state - the name of the input field
+ * @param {String} employeeFromData.department - the name of the input field
+ */
+ function HandleChangeOption(e, setEmployeeFromData, employeeFromData) {
+    const idChoice = e.currentTarget.selectedIndex
+  
+    const idText = {
+      name: e.target.id,
+      value: e.currentTarget.options[idChoice].text,
+    }
+    const { value, name } = idText
+  
+    setEmployeeFromData({
+      ...employeeFromData,
+      [name]: value,
+    })
+  }
+  
+  export default HandleChangeOption
+  
