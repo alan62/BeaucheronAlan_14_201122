0ec206dae8999d9f007581f393e6df268c07abf9
Alan62 <beaucheron.alan@free.fr>
2022-12-07 00:53:05 +0100

    fonction saveEmployee

---
+++ src/utils/saveEmployee.js
@@ -0,0 +1,27 @@
+/**
+ * Function to save employee data to the database
+ * @param {*} e - the event
+ * @param {Oject} employeeFromData - object with the input fields values
+ * @param {Object[]} employees - the array of the employees
+ * @param {string} employees.firstName
+ * @param {string} employees.lastName
+ * @param {string} employees.dateOfBirth
+ * @param {string} employees.startDate
+ * @param {string} employees.street
+ * @param {string} employees.city
+ * @param {string} employees.zipCode
+ * @param {string} employees.state
+ * @param {string} employees.department
+ * @param {*} setEmployees - function to set the state of the input fields
+ */
+ function SaveEmployee(e, employeeFromData, employees, setEmployees) {
+    e.preventDefault()
+    const newEmployees = [...employees]
+    newEmployees.push(employeeFromData)
+    console.log(newEmployees)
+    setEmployees(newEmployees)
+    document.getElementById('create-employee').reset()
+  }
+  
+  export default SaveEmployee
+  
