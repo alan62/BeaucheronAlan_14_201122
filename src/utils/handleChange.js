2dfd5cc988951ae96b593127b416625044873246
Alan62 <beaucheron.alan@free.fr>
2022-12-07 00:54:44 +0100

    fonction handleChange

---
+++ src/utils/handleChange.js
@@ -0,0 +1,35 @@
+/**
+ * Function to handle the change of the input fields
+ * @param {*} e - the event
+ * @param {*} setEmployeeFromData - function to set the state of the input fields
+ * @param {object} employeeFromData - object with the input fields values
+ * @param {String} employeeFromData.firstName - the name of the input field
+ * @param {String} employeeFromData.lastName - the name of the input field
+ * @param {String} employeeFromData.dateOfBirth - the name of the input field
+ * @param {String} employeeFromData.startDate - the name of the input field
+ * @param {String} employeeFromData.street - the name of the input field
+ * @param {String} employeeFromData.city - the name of the input field
+ * @param {String} employeeFromData.zipCode - the name of the input field
+ * @returns {object} employeeFromData
+ */
+
+ function HandleChange(e, setEmployeeFromData, employeeFromData) {
+    let { value, name } = e.currentTarget
+  
+    if (name === 'dateOfBirth' || name === 'startDate') {
+      const date = new Date(value)
+      const day = date.getDate()
+      const month = date.getMonth() + 1
+      const year = date.getFullYear()
+      const newDate = `${day}/${month}/${year}`
+      value = newDate
+    }
+  
+    setEmployeeFromData({
+      ...employeeFromData,
+      [name]: value,
+    })
+  }
+  
+  export default HandleChange
+  
